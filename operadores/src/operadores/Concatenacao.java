d27f03adc7e6402f6c53341943e9b8cae597a4d1
jessicatinguely <jessicatinguely@icloud.com>
2025-09-12 20:52:32 -0300

    Projetos operadores, ajustes e formatacao

--- operadores/src/operadores/Concatenacao.java
+++ operadores/src/operadores/Concatenacao.java
@@ -3,12 +3,12 @@ package operadores;
 public class Concatenacao {
 
 	public static void main(String[] args) {
-		
+
 		String texto1, texto2;
-		
+
 		texto1 = "2";
 		texto2 = "2";
-		
+
 		System.out.println(texto1 + texto2);
 
 	}
