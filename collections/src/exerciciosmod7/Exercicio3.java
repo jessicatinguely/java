2bf9ad05f65cf8065ca6a67ea3bd709271cab5e1
jessicatinguely <jessicatinguely@icloud.com>
2025-09-18 17:57:46 -0300

    Correção nome das classes

--- collections/src/exerciciosmod7/Exercicio3.java
+++ collections/src/exerciciosmod7/Exercicio3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class exercicio3 {
+public class Exercicio3 {
 
     public static void main(String[] args) {
 
--- collections/src/exerciciosmod7/Exercicio4.java
+++ collections/src/exerciciosmod7/Exercicio4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.HashSet;
 import java.util.Set;
 
-public class exercicio4 {
+public class Exercicio4 {
 
 	 public static void main(String[] args) {
 
